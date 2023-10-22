162da0a57262d615b9ef1a618c3e79b34775ea75
javierdm01 <diazmolinajavier04@gmail.com>
2023-10-22 17:42:24 +0200

    Mapa and Pacman charger

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ let casillaPuntos=[
     [1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1],
     [1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1],
     [1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1],
-    [1,1,1,0,0,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1],
+    [1,1,1,0,0,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,0,0,1,1,1],
     [0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0],
     [0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0],
     [1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1],
@@ -40,7 +40,7 @@ const cargarJuego=(e)=>{
         rows.id='rows'
         rows.classList.toggle('rows__game')
         game.appendChild(rows)
-        for(let j=0;j<25;j++){
+        for(let j=0;j<26;j++){
             let punto=document.createElement("a");
             if(casillaPuntos[i][j]==1){
                 punto.textContent='.'
@@ -48,10 +48,32 @@ const cargarJuego=(e)=>{
             }else{
                 punto.textContent=' '
             }
-                punto.classList.toggle('punto')
-                rows.appendChild(punto)
+            punto.classList.toggle('punto')
+            if (casillaPuntos[i][j]==2) {
+                let createPacman=document.createElement("a")
+                createPacman.id='pacman'
+                createPacman.classList.toggle('pacman')
+                console.log('hola');
+                rows.appendChild(createPacman)
+                posicionPacma=[i,j];
+            }
+            rows.appendChild(punto)
+            
         }
     }
 }
 
+const comprobarColisiones=(e)=>{
+    let tecla=e.key;
+    console.log(tecla);
+    if (tecla==='ArrowUp') {
+        if (casillaPuntos[p][posicionPacma[1]]==0) {
+            posicionPacma[0]--;
+        }
+    }
+}
+
+let posicionPacma=[];
+document.addEventListener("keydown",comprobarColisiones)
 document.addEventListener('DOMContentLoaded',cargarJuego)
+let pacman=document.getElementById('pacman')
