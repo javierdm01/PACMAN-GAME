bb9eb6ee442f7ee9cc9265e20374ca993354412b
javierdm01 <diazmolinajavier04@gmail.com>
2023-10-22 18:00:01 +0200

    CheckColisions

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ let casillaPuntos=[
     [1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1],
     [1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1],
     [1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1],
-    [1,1,1,0,0,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,0,0,1,1,1],
+    [1,1,1,0,0,1,1,1,1,1,1,1,0,2,1,1,1,1,1,1,1,0,0,1,1,1],
     [0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0],
     [0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0],
     [1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1],
@@ -51,7 +51,7 @@ const cargarJuego=(e)=>{
             punto.classList.toggle('punto')
             if (casillaPuntos[i][j]==2) {
                 let createPacman=document.createElement("a")
-                createPacman.id='pacman'
+                createPacman.id="pacman"
                 createPacman.classList.toggle('pacman')
                 console.log('hola');
                 rows.appendChild(createPacman)
@@ -65,11 +65,48 @@ const cargarJuego=(e)=>{
 
 const comprobarColisiones=(e)=>{
     let tecla=e.key;
-    console.log(tecla);
+    console.log(posicionPacma[0]);
+    let p=posicionPacma[0]-1
     if (tecla==='ArrowUp') {
-        if (casillaPuntos[p][posicionPacma[1]]==0) {
+        if (casillaPuntos[p][posicionPacma[1]]!=0) {
+            casillaPuntos[posicionPacma[0]][posicionPacma[1]]=0
             posicionPacma[0]--;
+            casillaPuntos[posicionPacma[0]][posicionPacma[1]]=2
         }
+    }
+    switch (tecla) {
+        case 'ArrowUp':
+            if (casillaPuntos[p][posicionPacma[1]]!=0 || (casillaPuntos[p][posicionPacma[1]]!=null)) {
+                casillaPuntos[posicionPacma[0]][posicionPacma[1]]=0
+                posicionPacma[0]--;
+                casillaPuntos[posicionPacma[0]][posicionPacma[1]]=2
+            }
+            break;
+        case 'ArrowDown':
+            if (casillaPuntos[p][posicionPacma[1]]!=0 || (casillaPuntos[p][posicionPacma[1]]!=null)) {
+                casillaPuntos[posicionPacma[0]][posicionPacma[1]]=0
+                posicionPacma[0]--;
+                casillaPuntos[posicionPacma[0]][posicionPacma[1]]=2
+                
+            }
+            break;
+        case 'ArrowLeft':
+            if (casillaPuntos[p][posicionPacma[1]]!=0 || (casillaPuntos[p][posicionPacma[1]]!=null)) {
+                casillaPuntos[posicionPacma[0]][posicionPacma[1]]=0
+                posicionPacma[0]--;
+                casillaPuntos[posicionPacma[0]][posicionPacma[1]]=2
+            }
+            break; 
+        case 'ArrowRight':
+            if (casillaPuntos[p][posicionPacma[1]]!=0 || (casillaPuntos[p][posicionPacma[1]]!=null)) {
+                casillaPuntos[posicionPacma[0]][posicionPacma[1]]=0
+                posicionPacma[0]--;
+                casillaPuntos[posicionPacma[0]][posicionPacma[1]]=2
+            }
+            break;
+
+        default:
+            break;
     }
 }
 
